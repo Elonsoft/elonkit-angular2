1f537306421da83c1e3a0dc20636a1907b7c17d4
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-27 15:33:03 +0400

    feat(alert): add new component

--- projects/elonkit/src/ui/alert/__stories__/alert-story-icon-mapping/index.ts
+++
@@ -1,3 +0,0 @@
-export { AlertStoryIconMappingComponent } from './alert-story-icon-mapping.component';
-export { AlertStoryIconMappingModule } from './alert-story-icon-mapping.module';
-export { ALERT_STORY_ICON_MAPPING_SOURCE } from './alert-story-icon-mapping.source';
