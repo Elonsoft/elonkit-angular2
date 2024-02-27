86462fd869cda5fe6e1b5fb59d891ac9f1398254
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-27 15:33:03 +0400

    feat(range-measure-select): add new component

---
+++ projects/elonkit/src/ui/range-measure-select/public-api.ts
@@ -0,0 +1,3 @@
+export { RangeMeasureSelectModule } from './range-measure-select..module';
+export { RangeMeasureSelectComponent } from './range-measure-select.component';
+export { IRangeSelect, IRangeOption, ERangeOption } from './range-measure-select.interface';
