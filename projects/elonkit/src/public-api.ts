90ab1e07e292600bf8ddaf55209247643f9e1261
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-27 15:33:03 +0400

    feat(kbd): add new component

--- projects/elonkit/src/public-api.ts
+++ projects/elonkit/src/public-api.ts
@@ -18,6 +18,7 @@ export * from './ui/tooltip';
 export * from './ui/spinner';
 export * from './ui/empty-state-compact';
 export * from './ui/range-measure-select';
+export * from './ui/kbd';
 
 export * from './ui/icons';
 export * from './ui/locale';
