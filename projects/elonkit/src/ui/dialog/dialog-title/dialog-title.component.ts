a0efa509f806d126db0aeaca6d73d1f383c94138
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-27 15:33:03 +0400

    feat(dialog): add new component

---
+++ projects/elonkit/src/ui/dialog/dialog-title/dialog-title.component.ts
@@ -0,0 +1,12 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'es-dialog-title',
+  templateUrl: './dialog-title.component.html',
+  styleUrls: ['./dialog-title.component.scss']
+})
+export class DialogTitleComponent {
+
+  @Input()
+  sticky: boolean = true;
+}
---
+++ projects/elonkit/src/ui/dialog/public-api.ts
@@ -0,0 +1,5 @@
+export { DialogModule } from './dialog.module';
+export { DialogComponent } from './dialog.component';
+export { DialogActionsComponent } from './dialog-actions/dialog-actions.component';
+export { DialogContentComponent } from './dialog-content/dialog-content.component';
+export { DialogTitleComponent } from './dialog-title/dialog-title.component';
