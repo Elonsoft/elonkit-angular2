7f36431e1add17bd54b4c69657985bb4b5f27cb0
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-27 15:33:03 +0400

    feat(empty-state-compact): add new component

---
+++ projects/elonkit/src/ui/empty-state-compact/empty-state-compact.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ESEmptyStateCompactComponent } from './empty-state-compact.component';
+
+@NgModule({
+  declarations: [ESEmptyStateCompactComponent],
+  imports: [CommonModule],
+  exports: [ESEmptyStateCompactComponent],
+})
+export class ESEmptyStateCompactModule {}
