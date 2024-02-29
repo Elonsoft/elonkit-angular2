9f25588d589cd79fc8c2be3e2f0d058f5e3b6104
Boris Novikov <b.novikov@elonsoft.ru>
2024-02-29 18:04:14 +0400

    feat(front): finish component, clean useless tests TODO: fix stories.mdx ang global styles

--- projects/elonkit/src/ui/action-heading/public-api.ts
+++ projects/elonkit/src/ui/action-heading/public-api.ts
@@ -1,7 +1,7 @@
 export { ESActionHeadingComponent } from './action-heading.component';
-export { ESHeadingMainComponent } from './heading-main/heading-main.component';
-export { ESHeadingBreadcrumbsComponent } from './heading-breadcrumbs/heading-breadcrumbs.component';
-export { ESHeadingHeaderComponent } from './heading-header/heading-header.component';
-export { ESHeadingActionsComponent } from './heading-actions/heading-actions.component';
-export { ESHeadingStatusComponent } from './heading-status/heading-status.component';
+export { ESHeadingMainComponent} from './heading-main/heading-main.component';
+export { ESHeadingBreadcrumbsComponent} from './heading-breadcrumbs/heading-breadcrumbs.component';
+export { ESHeadingHeaderComponent} from './heading-header/heading-header.component';
+export { ESHeadingActionsComponent} from './heading-actions/heading-actions.component';
+export { ESHeadingStatusComponent} from './heading-status/heading-status.component';
 export { ESActionHeadingModule } from './action-heading.module';
