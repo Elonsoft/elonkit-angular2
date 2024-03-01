d490a3ba92a100a62fc7244c2505d9ee165b68f5
Boris Novikov <b.novikov@elonsoft.ru>
2024-03-01 19:09:24 +0400

    feat: fix material and palets, change story of heading, add some styles to heading

--- projects/elonkit/src/ui/action-heading/heading-header/heading-header.component.ts
+++ projects/elonkit/src/ui/action-heading/heading-header/heading-header.component.ts
@@ -3,9 +3,21 @@ import {
   ChangeDetectionStrategy,
   ViewEncapsulation,
   Input,
+  InjectionToken,
+  Optional,
+  Inject,
 } from '@angular/core';
 
+export interface ESHeaderDefaultOptions {
+  typography?: string;
+  maxLines?: number;
+}
 
+const DEFAULT_TYPOGRAPHY = 'es-h2';
+
+export const ES_HEADER_DEFAULT_OPTIONS = new InjectionToken<ESHeaderDefaultOptions>(
+  'ES_ALERT_DEFAULT_OPTIONS'
+);
 @Component({
   selector: 'es-heading-header',
   templateUrl: './heading-header.component.html',
@@ -13,7 +25,36 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None
 })
+
 export class ESHeadingHeaderComponent {
   @Input()
   public maxLines = 1;
+
+  // TODO: Оформить maxLines с геттером и сетторм. Стилизовать остальные компоеннты хедера. Переделать стори.
+
+  private _typography: string;
+
+  /**
+   * Class applied to text.
+   */
+  @Input()
+  public get typography(): string {
+    return this._typography;
+  }
+  public set typography(value: string) {
+    this._typography =
+      value || (this.defaultOptions && this.defaultOptions.typography) || DEFAULT_TYPOGRAPHY;
+  }
+
+  constructor(
+        /**
+         * @internal
+         */
+        @Optional()
+        @Inject(ES_HEADER_DEFAULT_OPTIONS)
+        private defaultOptions: ESHeaderDefaultOptions
+  ) {
+    this.typography =
+    (defaultOptions && defaultOptions.typography) || DEFAULT_TYPOGRAPHY;
+  }
 }
