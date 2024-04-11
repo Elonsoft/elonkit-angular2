e3e917d9bab865b0658d5ce16cdb34b12396bfa2
Boris Novikov <b.novikov@elonsoft.ru>
2024-04-11 20:40:13 +0400

    feat(breadcrumbs): add new component

--- projects/elonkit/src/ui/breadcrumbs/breadcrumbs.module.ts
+++ projects/elonkit/src/ui/breadcrumbs/breadcrumbs.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
@@ -8,14 +7,12 @@ import { MatMenuModule } from '@angular/material/menu';
 import { MatRippleModule } from '@angular/material/core';
 
 import { ESBreadcrumbsComponent } from './breadcrumbs.component';
-import { ESBreadcrumbsResolver } from './breadcrumbs.resolver';
 
 import { ESBreadcrumbsBreadcrumbComponent } from './components/breadcrumbs-breadcrumb';
 import { ESBreadcrumbsCollapseComponent } from './components/breadcrumbs-collapse';
 
 import { ESBreadcrumbsMoreDirective } from './directives/breadcrumbs-more.directive';
 import { ESBreadcrumbsSeparatorDirective } from './directives/breadcrumbs-separator.directive';
-import { ESBreadcrumbsBackDirective } from './directives/breadcrumbs-back.directive';
 
 @NgModule({
   declarations: [
@@ -26,10 +23,9 @@ import { ESBreadcrumbsBackDirective } from './directives/breadcrumbs-back.direct
 
     ESBreadcrumbsMoreDirective,
     ESBreadcrumbsSeparatorDirective,
-    ESBreadcrumbsBackDirective,
   ],
-  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule, MatMenuModule, MatRippleModule],
-  exports: [ESBreadcrumbsComponent, ESBreadcrumbsMoreDirective, ESBreadcrumbsSeparatorDirective, ESBreadcrumbsBackDirective],
-  providers: [ESBreadcrumbsResolver],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatMenuModule, MatRippleModule],
+  exports: [ESBreadcrumbsComponent, ESBreadcrumbsMoreDirective, ESBreadcrumbsSeparatorDirective],
+  providers: [],
 })
 export class ESBreadcrumbsModule {}
---
+++ projects/elonkit/src/ui/breadcrumbs/breadcrumbs.stories.ts
@@ -0,0 +1,76 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+
+import { ESBreadcrumbsComponent } from './';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '~storybook/core.module';
+import { HttpClientModule } from '@angular/common/http';
+import { DemoWrapperComponent, DemoWrapperModule } from './demo-wrapper';
+
+const meta: Meta<DemoWrapperComponent> = {
+  component: DemoWrapperComponent,
+  tags: ['autodocs'],
+  args: {
+    breadcrumbs: [
+      {
+        callback: () => {
+          console.log('home clicked');
+        },
+        data: {
+          label: 'Home',
+          ariaLabel: 'Home',
+        },
+      },
+      {
+        callback: () => {
+          console.log('gallery clicked');
+        },
+        data: {
+          label: 'Gallery',
+          ariaLabel: 'gallery',
+        },
+      },
+      {
+        callback: () => {
+          console.log('images clicked');
+        },
+        data: {
+          label: 'Images',
+          ariaLabel: 'Images',
+          svgIcon: 'es-48:image-fill-w500-lc',
+        },
+      },
+      {
+        callback: () => {
+          console.log('info clicked');
+        },
+        data: {
+          label: 'Info',
+          ariaLabel: 'Info',
+          svgIcon: 'es-48:information-fill-w500-lc',
+        },
+      },
+    ],
+  },
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<ESBreadcrumbsComponent>;
+
+export const Primary: Story = {
+  name: 'Demo',
+  render: (args, context) => ({
+    template: `
+    <demo-wrapper
+    [breadcrumbs]="breadcrumbs"
+    ></demo-wrapper>
+    `,
+    props: {
+      ...args,
+    },
+    moduleMetadata: {
+      imports: [DemoWrapperModule, HttpClientModule, CoreModule, BrowserAnimationsModule],
+    },
+  }),
+};
