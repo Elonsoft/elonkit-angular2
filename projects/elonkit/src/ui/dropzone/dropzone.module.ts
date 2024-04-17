6c9a2f33236a212c7f47a6b19b69049d28c6bd24
Boris Novikov <b.novikov@elonsoft.ru>
2024-04-17 11:52:15 +0400

    feat(dropzone): add new component

--- projects/elonkit/src/ui/dropzone/dropzone.module.ts
+++ projects/elonkit/src/ui/dropzone/dropzone.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { MatRippleModule } from '@angular/material/core';
 
@@ -11,5 +11,6 @@ import { ESDropzoneComponent } from './dropzone.component';
   declarations: [ESDropzoneComponent],
   imports: [CommonModule, MatButtonModule, MatIconModule, MatRippleModule, HttpClientModule],
   exports: [ESDropzoneComponent],
+  providers: [MatIconRegistry],
 })
 export class ESDropzoneModule {}
