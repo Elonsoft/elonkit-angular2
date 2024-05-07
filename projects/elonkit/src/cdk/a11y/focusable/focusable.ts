3c03eaac554a212253c9b67e6dc294199edbbc10
Boris Novikov <b.novikov@elonsoft.ru>
2024-05-07 14:52:40 +0400

    feat: setup cspell, add ignire, add rus dict and add words

--- projects/elonkit/src/cdk/a11y/focusable/focusable.ts
+++ projects/elonkit/src/cdk/a11y/focusable/focusable.ts
@@ -1,23 +1,23 @@
 export const FOCUSABLE = 'a, button, input, textarea, select, details,[tabindex]:not([tabindex="-1"])';
 
-export function getFocusableElemets(parent: Document | HTMLElement): HTMLElement[] {
+export function getFocusableElements(parent: Document | HTMLElement): HTMLElement[] {
   return Array.from(parent.querySelectorAll(FOCUSABLE)).filter((e) => !e.hasAttribute('disabled')) as HTMLElement[];
 }
 
 export function getInnerFocusableElement(element: HTMLElement) {
-  const elements = getFocusableElemets(element);
+  const elements = getFocusableElements(element);
 
   return (elements[0] as HTMLElement) || null;
 }
 
 export function getPrevFocusableElement(element: HTMLElement) {
-  const elements = getFocusableElemets(document);
+  const elements = getFocusableElements(document);
 
   return elements.find((e) => element.compareDocumentPosition(e) === Node.DOCUMENT_POSITION_PRECEDING);
 }
 
 export function getNextFocusableElement(element: HTMLElement) {
-  const elements = getFocusableElemets(document);
+  const elements = getFocusableElements(document);
 
   return elements.find((e) => element.compareDocumentPosition(e) === Node.DOCUMENT_POSITION_FOLLOWING);
 }
