a3b3fce2ad3f483d1439735a052186a539a05983
Boris Novikov <b.novikov@elonsoft.ru>
2024-05-24 14:10:01 +0400

    feat: change resize methods, add unlistenting, change vars to subjects

--- projects/elonkit/src/ui/sidebar/sidebar.component.ts
+++ projects/elonkit/src/ui/sidebar/sidebar.component.ts
@@ -1,16 +1,16 @@
 import {
   ChangeDetectionStrategy,
-  ChangeDetectorRef,
   Component,
   ElementRef,
   EventEmitter,
-  HostListener,
   Input,
+  OnDestroy,
   Output,
   Renderer2,
   ViewChild,
   ViewEncapsulation,
 } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'es-sidebar',
@@ -19,7 +19,7 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
 })
-export class ESSidebarComponent {
+export class ESSidebarComponent implements OnDestroy{
   @Input() color: 'default' | 'primary' | 'secondary' = 'default';
   @Input() maxWidth = 400;
   @Input() minWidth = 220;
@@ -29,21 +29,20 @@ export class ESSidebarComponent {
   @Output() widthChange = new EventEmitter<number>();
   @Output() widthChangeCommit = new EventEmitter<number>();
 
-  public isMouseMove = false;
-  public isMouseDown = false;
+  public isMouseMove$ = new BehaviorSubject(false);
+  public isMouseDown$ = new BehaviorSubject(false);
 
   public screenX: number | null = null;
-  public isActive: boolean | null = null;
-  // public onMouseMove: (event: any) => void | null = null;
+  private initialWidth: number | null = null;
+
+  private removeEventListeners: (() => void)[] = [];
 
   constructor(
     private renderer: Renderer2,
-    private cd: ChangeDetectorRef
   ) {}
 
-  @HostListener('window:resize')
-  _onResize(): void {
-    // console.log('resize', this.content.nativeElement.clientWidth)
+  public ngOnDestroy(): void {
+    this.removeAllEventListeners()
   }
 
   @ViewChild('content', { static: true }) private content: ElementRef;
@@ -51,45 +50,55 @@ export class ESSidebarComponent {
   public _onMouseDown(event: MouseEvent): void {
     if (event.button === 0 && this.isOpen) {
       this.screenX = event.screenX;
-      this.isMouseDown = true;
+      this.isMouseDown$.next(true);
+      this.initialWidth = this.content.nativeElement.getBoundingClientRect().width;
 
-      this.renderer.listen('document', 'mousemove', this._onMouseMove.bind(this));
-      this.renderer.listen('document', 'mouseup', this._onMouseUp.bind(this));
+      this.removeEventListeners.push(
+        this.renderer.listen('document', 'mousemove', this._onMouseMove.bind(this)),
+        this.renderer.listen('document', 'mouseup', this._onMouseUp.bind(this))
+      );
     }
   }
 
   public _onTouchStart(event: TouchEvent): void {
     if (this.isOpen) {
       this.screenX = event.touches[0].screenX;
-      this.isMouseDown = true;
-      // this.isActive = true;
+      this.isMouseDown$.next(true);
+      this.initialWidth = this.content.nativeElement.getBoundingClientRect().width;
 
-      this.renderer.listen('document', 'touchmove', this._onTouchMove.bind(this));
-      this.renderer.listen('document', 'touchend', this._onTouchEnd.bind(this));
+      this.removeEventListeners.push(
+        this.renderer.listen('document', 'touchmove', this._onTouchMove.bind(this)),
+        this.renderer.listen('document', 'touchend', this._onTouchEnd.bind(this))
+      );
     }
   }
 
   private _onMouseMove(event: MouseEvent): void {
-    if (this.screenX !== null && this.content.nativeElement) {
-      const width = this.content.nativeElement.getBoundingClientRect().width + (event.screenX - this.screenX);
-      console.log(Math.min(this.maxWidth, Math.max(width, this.minWidth)));
+    if (this.screenX !== null  && this.initialWidth !== null && this.content.nativeElement) {
+      const deltaX = event.screenX - this.screenX;
+      const newWidth = this.initialWidth + deltaX;
+      const clampedWidth = Math.min(this.maxWidth, Math.max(newWidth, this.minWidth));
 
-      this.content.nativeElement.style.width = `${Math.min(this.maxWidth, Math.max(width, this.minWidth))}px`;
-      this.widthChange.emit(width);
-      // this.screenX = event.screenX; // ?
+      this.content.nativeElement.style.width = `${clampedWidth}px`;
+
+      this.widthChange.emit(clampedWidth);
     }
 
-    this.isMouseMove = true;
+    this.isMouseMove$.next(true);
   }
 
   private _onTouchMove(event: TouchEvent): void {
-    if (this.screenX !== null && this.content.nativeElement) {
-      const width = this.content.nativeElement.getBoundingClientRect().width + (event.touches[0].screenX - this.screenX);
+    if (this.screenX !== null && this.initialWidth !== null && this.content.nativeElement) {
+      const deltaX = event.touches[0].screenX - this.screenX;
+      const newWidth = this.initialWidth + deltaX;
+      const clampedWidth = Math.min(this.maxWidth, Math.max(newWidth, this.minWidth));
 
-      // this.content.nativeElement.style.width = `${Math.min(this.maxWidth, Math.max(width, this.minWidth))}px`;
-      this.widthChange.emit(width);
-      // this.screenX = event.touches[0].screenX; // ?
+      this.content.nativeElement.style.width = `${clampedWidth}px`;
+
+      this.widthChange.emit(clampedWidth);
     }
+
+    this.isMouseMove$.next(true);
   }
 
   private _onMouseUp(): void {
@@ -101,12 +110,20 @@ export class ESSidebarComponent {
   }
 
   private finalizeResize(): void {
-    if (this.screenX !== null && this.content.nativeElement) {
+    if (this.initialWidth !== null && this.content.nativeElement) {
       this.widthChangeCommit.emit(Math.ceil(this.content.nativeElement.getBoundingClientRect().width));
     }
-    this.isMouseMove = false;
-    this.isMouseDown = false;
+
+    this.isMouseMove$.next(false);
+    this.isMouseDown$.next(false);
     this.screenX = null;
-    this.cd.detectChanges();
+    this.initialWidth = null;
+
+    this.removeAllEventListeners();
+  }
+
+  private removeAllEventListeners() {
+    this.removeEventListeners.forEach(removeListener => removeListener());
+    this.removeEventListeners = [];
   }
 }
