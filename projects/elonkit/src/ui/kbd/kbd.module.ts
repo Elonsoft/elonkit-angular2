8e92b5d15578c637bf68b8f9b7990dc7d985e037
Руслан <ruslankko@gmail.com>
2024-06-07 11:21:09 +0500

    Fixed Prettier

--- projects/elonkit/src/ui/kbd/kbd.module.ts
+++ projects/elonkit/src/ui/kbd/kbd.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { KbdComponent } from './kbd.component';
 
-
 @NgModule({
   declarations: [KbdComponent],
   imports: [CommonModule],
-  exports: [KbdComponent]
+  exports: [KbdComponent],
 })
-export class KbdModule { }
+export class KbdModule {}
